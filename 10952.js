649b29dab155b8afe8c95256cea2c66567b9d7d0
김동현 <kimdonghyun@gimdonghyeon-ui-MacBookPro.local>
2019-09-30 00:15:12 +0900

    10952 해결

---
+++ 10952.js
@@ -0,0 +1,17 @@
+fs = require('fs'); 
+var input = fs.readFileSync('/Users/kimdonghyun/git/baekjoon/test.txt').toString().split("\n");
+
+var num = input
+for(var i=0; i<num.length; i++){
+	var numArray = num[i].split(' ')
+	var a = parseInt(numArray[0])
+	var b = parseInt(numArray[1])
+
+	if(0 < a && b < 10){
+		var result = a+b
+	
+		if(i<num.length-1){
+			console.log(result)
+		}
+	}
+}
