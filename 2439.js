954e741133a97d1b12b16eed95bf63f9ebee4922
김동현 <kimdonghyun@gimdonghyeon-ui-MacBookPro.local>
2019-10-05 15:35:30 +0900

    2439 별찍기2

--- 2439.js
+++ 2439.js
@@ -2,10 +2,15 @@ fs = require('fs');
 var input = fs.readFileSync('/Users/kimdonghyun/git/baekjoon/test.txt').toString().split("\n");
 
 var num = parseInt(input[0])
-var n=1
 var result = ''
+var space = ''
 if(1 <= num <= 100){
-	for(n; n<num+1; n++){
-		console.log(result+='*')
+	for(var n = 0; n < num; n++){
+		for(var i = num-1; i>n ;i--){
+			space += ' '
+		}
+		result += '*'
+		console.log(space+result)
+		space=''
 	}
 }
